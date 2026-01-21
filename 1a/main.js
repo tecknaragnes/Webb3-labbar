dede4376c79bf01048c974574c7cbbfaa3ac7940
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-01-21 14:20:34 +0100

    Labb1a - initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,8 @@
+import * as Math from "./math.js";
+
+console.log(Math.add(2, 7));
+console.log(Math.subtract(18487, 568));
+console.log(Math.multiply(23, 67));
+console.log(Math.divide(178, 8));
+console.log("I want pie! " + Math.Pi);
+console.log("The answer to life is " + Math.secretSauce);
---
+++ 1a/math.js
@@ -0,0 +1,18 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    return a / b;
+}
+
+export const Pi = 3.14159;
+export const secretSauce = 42;
