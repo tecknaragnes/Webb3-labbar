db6a1c26d7a6fd6507576ba0172a6976a2fd11ed
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-02 15:42:40 +0100

    Finish labb3a

---
+++ 3a/main.js
@@ -0,0 +1,17 @@
+const double = (n) => {
+    return n * 2;
+}
+
+const isEven = n =>
+    n % 2 === 0;
+
+const sum = (a, b) => a + b; //denna är bäst
+
+console.log(double(2));
+console.log(isEven(5));
+console.log(sum(2, 7));
+
+const square = x => x * x;
+const sayHello = () => "Hej!";
+const multiply = (a, b) => a * b;
+const increment = n => n + 1;
