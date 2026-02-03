aafe3b65d01c812678a925e2dd740bd99236b9b7
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-03 11:22:32 +0100

    Finish labb3c

---
+++ 3c/main.js
@@ -0,0 +1,16 @@
+// const response = await fetch("menu.json");
+
+fetch("menu.json")
+    .then(response => {
+        console.log(response);
+        console.log("Status: ", response.status);
+        console.log("OK? ", response.ok);
+        console.log("URL: ", response.url);
+        return response.json();
+    })
+    .then(menu => {
+        console.log(menu);
+        console.log("Restaurangens namn: ", menu.name);
+        console.log("Antal kategorier: ", menu.categories.length);
+        console.log(menu.categories[1].dishes[0].name);
+    })
