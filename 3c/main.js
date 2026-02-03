3051818fee8ef4c43513e4df60da1b8b7408f154
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-03 11:24:24 +0100

    Actually finish labb3c

--- 3c/main.js
+++ 3c/main.js
@@ -1,16 +1,17 @@
-// const response = await fetch("menu.json");
+const response = await fetch("menu.json");
+const menu = await response.json();
 
-fetch("menu.json")
-    .then(response => {
-        console.log(response);
-        console.log("Status: ", response.status);
-        console.log("OK? ", response.ok);
-        console.log("URL: ", response.url);
-        return response.json();
-    })
-    .then(menu => {
-        console.log(menu);
-        console.log("Restaurangens namn: ", menu.name);
-        console.log("Antal kategorier: ", menu.categories.length);
-        console.log(menu.categories[1].dishes[0].name);
-    })
+// fetch("menu.json")
+//     .then(response => {
+//         console.log(response);
+//         console.log("Status: ", response.status);
+//         console.log("OK? ", response.ok);
+//         console.log("URL: ", response.url);
+//         return response.json();
+//     })
+//     .then(menu => {
+console.log(menu);
+console.log("Restaurangens namn: ", menu.name);
+console.log("Antal kategorier: ", menu.categories.length);
+console.log(menu.categories[1].dishes[0].name);
+// })
