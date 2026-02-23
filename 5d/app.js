b5c0ebd71d2f793b9defd2afba8e395d091bea64
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-23 09:41:07 +0100

    Add multiply function

--- 5d/app.js
+++ 5d/app.js
@@ -2,12 +2,16 @@
 function greet(name) {
     return `Hej ${name}!`;
 }
-
 console.log(greet("Världen"));
 
 
 function farewell(name) {
     return `Hejdå ${name}!`;
 }
-
 console.log(farewell("Världen"));
+
+
+function multiply(a, b) {
+    return a + b; // Obs: detta är fel! Borde vara a * b
+}
+console.log(multiply(3, 4)); // Skriver ut 7 istället för 12
